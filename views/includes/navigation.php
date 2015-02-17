09f6ab478ab69e6c5ea964863735cc64631c8fe0
DewSee <veckler08@gmail.com>
2015-02-17 14:01:11 +0100

    navigáció formázás

--- views/includes/navigation.php
+++ views/includes/navigation.php
@@ -1,6 +1,7 @@
     <ul id="navigation" class="nav nav-pills">
       <li role="presentation"><a href="?q=kezdolap">Kezdőlap</a></li>
-      <li role="presentation"><a href="?q=bemutatkozas">Bemutatkozás</a></li>
-      <li role="presentation"><a href="?q=kepgaleria">Képgaléria</a></li>
+      <li role="presentation"><a href="?q=cegunkrol">Cégünkről</a></li>
+      <li role="presentation"><a href="?q=szolgaltatasok">Szolgáltatások</a></li>
+      <li role="presentation"><a href="?q=galeria">Galéria</a></li>
       <li role="presentation"><a href="?q=kapcsolat">Kapcsolat</a></li>
     </ul>
